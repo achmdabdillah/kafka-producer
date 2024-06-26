96c49d6646e127bb4d5c996b3889b83465680c4d
achmdabdillah <achmdabdil9@gmail.com>
2024-06-26 16:08:44 +0700

    kafka producer template

--- src/main/java/br/com/example/broker/producer/TopicProducer.java
+++ src/main/java/br/com/example/broker/producer/TopicProducer.java
@@ -2,6 +2,10 @@ package br.com.example.broker.producer;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+
+import org.hibernate.mapping.Map;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -14,11 +18,131 @@ public class TopicProducer {
     @Value("${topic.name.producer}")
     private String topicName;
 
+    @Value("${topic.name.credit_approval}")
+    private String credit_approval;
+
+    @Value("${topic.name.credit_approval_cancel}")
+    private String credit_approval_cancel;
+
+    @Value("${topic.name.credit_approval_rtre}")
+    private String credit_approval_rtre;
+
+    @Value("${topic.name.kyc}")
+    private String kyc;
+
+    @Value("${topic.name.pooling_order}")
+    private String pooling_order;
+
+    @Value("${topic.name.reguler_survey}")
+    private String reguler_survey;
+
+    @Value("${topic.name.result_credit_approval_rtre}")
+    private String result_credit_approval_rtre;
+
+    @Value("${topic.name.screening_dua}")
+    private String screening_dua;
+
+    @Value("${topic.name.screening_tiga}")
+    private String screening_tiga;
+
+    @Value("${topic.name.return_pooling}")
+    private String return_pooling;
+
+    @Value("${topic.name.silent_survey}")
+    private String silent_survey;
+
+    @Value("${topic.name.submit_approval_brms}")
+    private String submit_approval_brms;
+
+    @Value("${topic.name.tele_survey}")
+    private String tele_survey;
+
+    @Value("${topic.name.efm}")
+    private String efm;
+
+    @Value("${topic.name.efd}")
+    private String efd;
+    
+    @Value("${topic.name.return_request_approve}")
+    private String return_request_approve;
+
+    @Value("${topic.name.credit_approval_cancel_po_cl}")
+    private String credit_approval_cancel_po_cl;
+
+    @Value("${topic.name.result_po_cl}")
+    private String result_po_cl;
+
     private final KafkaTemplate<String, String> kafkaTemplate;
 
-    public void send(String message){
-        log.info("Payload enviado: {}",  message);
-        kafkaTemplate.send(topicName, message);
+    public void send_credit_approval(String message) {
+        kafkaTemplate.send(credit_approval, message);
+    }
+
+    public void send_credit_approval_cancel(String message) {
+        kafkaTemplate.send(credit_approval_cancel, message);
+    }
+
+    public void send_credit_approval_rtre(String message) {
+        kafkaTemplate.send(credit_approval_rtre, message);
+    }
+
+    public void send_kyc(String message) {
+        kafkaTemplate.send(kyc, message);
+    }
+
+    public void send_pooling_order(String message) {
+        kafkaTemplate.send(pooling_order, message);
+    }
+
+    public void send_reguler_survey(String message) {
+        kafkaTemplate.send(reguler_survey, message);
+    }
+
+    public void send_result_credit_approval_rtre(String message) {
+        kafkaTemplate.send(result_credit_approval_rtre, message);
+    }
+
+    public void send_screening_dua(String message) {
+        kafkaTemplate.send(screening_dua, message);
+    }
+
+    public void send_screening_tiga(String message) {
+        kafkaTemplate.send(screening_tiga, message);
+    }
+
+    public void send_return_pooling(String message) {
+        kafkaTemplate.send(return_pooling, message);
+    }
+
+    public void send_silent_survey(String message) {
+        kafkaTemplate.send(silent_survey, message);
+    }
+
+    public void send_submit_approval_brms(String message) {
+        kafkaTemplate.send(submit_approval_brms, message);
+    }
+
+    public void send_tele_survey(String message) {
+        kafkaTemplate.send(tele_survey, message);
+    }
+
+    public void send_efm(String message) {
+        kafkaTemplate.send(efm, message);
+    }
+
+    public void send_efd(String message) {
+        kafkaTemplate.send(efd, message);
+    }
+    public void send_return_request_approve(String message) {
+        kafkaTemplate.send(return_request_approve, message);
+    }
+
+    public void send_credit_approval_cancel_po_cl(String message) {
+        kafkaTemplate.send(credit_approval_cancel_po_cl, message);
+    }
+    
+    public void send_result_po_cl(String message) {
+        kafkaTemplate.send(result_po_cl, message);
     }
 
 }
--- src/main/java/br/com/example/controller/KafkaController.java
+++ src/main/java/br/com/example/controller/KafkaController.java
@@ -2,17 +2,126 @@ package br.com.example.controller;
 
 import br.com.example.broker.producer.TopicProducer;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/kafka")
+@RequiredArgsConstructor
+@Slf4j
 public class KafkaController {
     private final TopicProducer topicProducer;
-    @GetMapping(value = "/send")
-    public void send(){
-        topicProducer.send("Mensagem de teste enviada ao tópico");
+
+    @PostMapping(value = "/send/credit_approval")
+    public String send_credit_approval(@RequestBody String body) {
+        topicProducer.send_credit_approval(body);
+        return "Success";
+    }
+
+    @PostMapping(value = "/send/credit_approval_cancel")
+    public String send_credit_approval_cancel(@RequestBody String body) {
+        topicProducer.send_credit_approval_cancel(body);
+        return "Success";
+    }
+
+    @PostMapping(value = "/send/credit_approval_rtre")
+    public String send_credit_approval_rtre(@RequestBody String body) {
+        topicProducer.send_credit_approval_rtre(body);
+        return "Success";
+    }
+
+    @PostMapping(value = "/send/kyc")
+    public String send_kyc(@RequestBody String body) {
+        topicProducer.send_kyc(body);
+        return "Success";
+    }
+
+    @PostMapping(value = "/send/pooling_order")
+    public String send_pooling_order(@RequestBody String body) {
+        topicProducer.send_pooling_order(body);
+        return "Success";
+    }
+
+    @PostMapping(value = "/send/reguler_survey")
+    public String send_reguler_survey(@RequestBody String body) {
+        topicProducer.send_reguler_survey(body);
+        return "Success";
+    }
+
+    @PostMapping(value = "/send/result_credit_approval_rtre")
+    public String send_result_credit_approval_rtre(@RequestBody String body) {
+        topicProducer.send_result_credit_approval_rtre(body);
+        return "Success";
+    }
+
+    @PostMapping(value = "/send/screening_dua")
+    public String send_screening_dua(@RequestBody String body) {
+        topicProducer.send_screening_dua(body);
+        return "Success";
+    }
+
+    @PostMapping(value = "/send/screening_tiga")
+    public String send_screening_tiga(@RequestBody String body) {
+        topicProducer.send_screening_tiga(body);
+        return "Success";
+    }
+
+    @PostMapping(value = "/send/return_pooling")
+    public String send_return_pooling(@RequestBody String body) {
+        topicProducer.send_return_pooling(body);
+        return "Success";
+    }
+
+    @PostMapping(value = "/send/silent_survey")
+    public String send_silent_survey(@RequestBody String body) {
+        topicProducer.send_silent_survey(body);
+        return "Success";
+    }
+
+    @PostMapping(value = "/send/submit_approval_brms")
+    public String send_submit_approval_brms(@RequestBody String body) {
+        topicProducer.send_submit_approval_brms(body);
+        return "Success";
+    }
+
+    @PostMapping(value = "/send/tele_survey")
+    public String send_tele_survey(@RequestBody String body) {
+        topicProducer.send_tele_survey(body);
+        return "Success";
+    }
+
+    @PostMapping(value = "/send/efm")
+    public String send_efm(@RequestBody String body) {
+        topicProducer.send_efm(body);
+        return "Success";
+    }
+
+    @PostMapping(value = "/send/efd")
+    public String send_efd(@RequestBody String body) {
+        topicProducer.send_efd(body);
+        return "Success";
+    }
+
+    @PostMapping(value = "/send/return_request_approve")
+    public String send_return_request_approve(@RequestBody String body) {
+        topicProducer.send_return_request_approve(body);
+        return "Success";
+    }
+
+    @PostMapping(value = "/send/credit_approval_cancel_po_cl")
+    public String send_credit_approval_cancel_po_cl(@RequestBody String body) {
+        topicProducer.send_credit_approval_cancel_po_cl(body);
+        return "Success";
+    }
+
+    @PostMapping(value = "/send/result_po_cl")
+    public String send_result_po_cl(@RequestBody String body) {
+        log.info("MASOK RESULT POCEL");
+        topicProducer.send_result_po_cl(body);
+        return "Success";
     }
 }
